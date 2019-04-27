66bfd621d371031c1ec50ba9b33844e753f33fe5
JacobLaing <jmlaing@iastate.edu>
2019-04-26 22:18:46 -0500

    Added navigation between different views from navigation drawer

---
+++ DQ Statistics/DQ Statistics/ContestViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ContestViewController.swift
+//  DQ Statistics
+//
+//  Created by Jacob Laing on 4/26/19.
+//  Copyright © 2019 Jacob Laing. All rights reserved.
+//
+
+import UIKit
+
+class ContestViewController: UIViewController {
+
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        sideMenus()
+        customizeNavBar()
+        // Do any additional setup after loading the view.
+    }
+    
+    func sideMenus() {
+        if revealViewController() != nil {
+            menuButton.target = revealViewController()
+            menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
+            revealViewController()?.rearViewRevealWidth = 250 //275
+            //revealViewController()?.rightViewRevealWidth = 0
+            
+            
+            view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+            
+        }
+    }
+    
+    func customizeNavBar() {
+        navigationController?.navigationBar.topItem?.title = "Contest"
+        navigationController?.navigationBar.tintColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1)
+        navigationController?.navigationBar.barTintColor = UIColor(red: 0/255, green: 122/255, blue: 193/255, alpha: 1)
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
